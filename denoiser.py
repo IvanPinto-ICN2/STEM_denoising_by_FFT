03e3958ded74e718705263c091529f1e171750ab
IvanPinto-ICN2 <ivan.pinto@icn2.cat>
2025-08-01 13:56:17 +0200

    Add sample test

--- denoiser.py
+++ denoiser.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 
 def fft(image, log = True):
@@ -10,24 +11,31 @@ def fft(image, log = True):
 
 def fft_denoiser(fft, model):
     prediction, coordinates = model.predict(fft)
-    pred_r = prediction[0]
+    pred_r = prediction[0, :, :, 0]
 
     return pred_r
 
 # Denoising trought ffts
 def fft_denoising(image, dl_model, all_process = True):
+    # Grayscale image
+    if len(image.shape) == 3:
+        image = image[:,:,0]
 
     # Original fft
     fft_original = np.fft.fftshift(np.fft.fft2(image))
+    print(fft_original.shape)
 
     # Log fft to apply the denoising
     log_fft = fft(image, log = True)
+    print(log_fft.shape)
 
     # Denoising prediction
-    fft_denoised = fft_denoiser(log_fft, dl_model, len(image))
+    fft_denoised = fft_denoiser(log_fft, dl_model)
+    print(fft_denoised.shape)
 
     # Denoise the image (IFFT)
     img_denoised = np.abs(np.fft.ifft2(fft_original * fft_denoised))
+    print(img_denoised.shape)
 
     if all_process:
         process = {'original_image' : image, 
